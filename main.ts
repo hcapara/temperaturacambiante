9c470ba587e89faeb477236229fb3676d3d0a997
hcapara <44587815+hcapara@users.noreply.github.com>
2022-09-29 19:55:25 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    basic.pause(1000)
+    basic.showNumber(input.temperature())
+    basic.pause(1000)
+    basic.showNumber(input.temperature())
+    basic.pause(1000)
+})
